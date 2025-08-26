ba6a94ab5d24833165458aa69077860b45a650f9
Coder-pro1 <sithijaseneviratne@gmail.com>
2025-08-26 06:38:46 +0530

    Setup

--- backend/index.js
+++ backend/index.js
@@ -7,11 +7,14 @@ app.use(express.json());
 app.use(cors());
 
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "13579Codgod",
-  database: "test",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 
